db0e14b2207f47c5fce5e79c75de4fff8d96afa0
pepsin <yechunlin@admaster.com.cn>
2014-04-04 11:24:15 +0800

    Done the basic Parsing

--- sketch_parse.js
+++ sketch_parse.js
@@ -1,109 +1,68 @@
-var BASIC_OPERATORS = ["+", "-", "×", "÷", "(", ")", "*", "/", "=", ","],
-    ADVANCE_OPERATORS = ["POW", "SQRT"],
-    LITERALS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
-    NUMBER = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-
-
-var GLOBAL_FUNCTIONS = {
-
-};
-
-var GLOBAL_VARIABLES = {
-};
-
-function isEqualSign(atom) {
-  return atom == "=";
-}
-
-function isFrontCurlyBracket(atom) {
-  return atom == "(";
-}
-
-function isBasicOperator(atom) {
-  return BASIC_OPERATORS.indexOf(atom) != -1;
-}
-
-function isLiterals(atom) {
-  return LITERALS.indexOf(atom) != -1;
-}
-
-function isNumber(atom) {
-  return NUMBER.indexOf(atom) != -1;
-}
-
-function Tokenize(string) {
-  console.log("FUNCTION IS", string);
-  string = string.replace(/ /g, "");
-  
-  var current = 0,
-      token,
-      token_arr = [],
-      prevIsNumber = false;
-  
-  function peek() {
-    return string[current + 1];
-  }
-  
-  function previous() {
-    return string[current - 1];
-  }
-  
-  function currentAtom() {
-    return string[current];
-  }
-  
-  while (current <= string.length) {
-    var atom = currentAtom(), prev = previous();
-    
-    if (isBasicOperator(atom)) {
-      if (!isBasicOperator(token)) {
-        prevIsNumber = false;
-        token_arr.push(token);
-      }
-      token = atom;
-      token_arr.push(token);
-    } else if (isLiterals(atom)){
-      if (isLiterals(prev)) {
-        token += atom;
-      } else {
-        token = atom;
-      }
-    } else if (isNumber(atom)) {
-      if (isBasicOperator(prev) || prev == undefined) {
-        prevIsNumber = true;
-        token = atom;
-      //Need to throw an error here when there's multiple "0", like 00.2 is illegal
-      } else if (isNumber(prev) && prevIsNumber) {
-        token += atom;
-      }
-    }
-    current += 1;
-  }
-  token_arr.push(token);
-  return token_arr;
-}
-
-var tokens = Tokenize("aFunction(x, y, z) = x + (POW(x, 20.1) + x) * 30.2 + 301");
-console.log(Tokenize("aFunction(x, y, z) = x + (POW(x, 000.1) + x) * 30.2 + 301"));
-console.log(tokens);
+var tokens = ["(","x", "+", "1", ")", "+", "(", "POW", "(", "x", ",", "000.1", ")", "+", "x", ")", "*", "30.2", "+", "301" ];
+var another_tokens = ["x", "+", "(", "POW", "(", "x", ",", "000.1", ")", "+", "x", ")", "*", "30.2", "+", "301" ];
+var tokens2 = ["POW", "(", "x", ",", "000.1", ")", "+", "x", "*", "30.2", "+", "301" ];
+LITERALS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+NUMBER = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+IDENTIFER = 0;
+ARITHMETIC = 1;
+NUMBER = 2;
 function Parse(tokens) {
-  if (tokens.indexOf("=") > -1) {
-    var equal_sign_position = tokens.indexOf("=");
-    var equation_left = tokens.slice(0, equal_sign_position);
-    var equation_right = tokens.slice(equal_sign_position + 1, tokens.length);
+  var left, right, symbol;
+  if (tokens.length < 2) {
+    return tokens[0];
   } else {
-    
-  }
-  //Split up the tokens base on the "equal"
-  
-  if (equation_left.indexOf("(") > -1) {
-    count_variables(equation_left);
-    GLOBAL_FUNCTIONS[function_name] = new Function(function_variables, function_body);
-  } else if (equation_left.length == 1 && equation_left[0].length > 0) {
-    GLOBAL_VARIABLES[equation_left[0]] = evaluate(equation_right);
-  } else {
-    
+    if (tokens[0] == "POW") {
+      var pow_end_position = tokens.indexOf(")")
+      var the_whole_pow_expr = tokens.slice(0, pow_end_position);
+      var comma_position = the_whole_pow_expr.indexOf(",");
+      left = {
+        name: "POW",
+        first: Parse(the_whole_pow_expr.slice(2, comma_position)),
+        second: Parse(the_whole_pow_expr.slice(comma_position + 1, the_whole_pow_expr.length))
+      }
+      var right_expr = tokens.slice(pow_end_position + 2, tokens.length);
+      right = Parse(right_expr);
+      symbol = tokens[pow_end_position + 1];
+    } else if (tokens[0] != "(") {
+      var left_expr = [tokens[0]];
+      var right_expr = tokens.slice(2, tokens.length);
+      left = Parse(left_expr);
+      right = Parse(right_expr);
+      symbol = tokens[1];
+    } else {
+      var left_expr_end_position = FindCloseParenthesisPosition(tokens.slice(0, tokens.length));
+      left = Parse(tokens.slice(1, left_expr_end_position));
+      right = Parse(tokens.slice(left_expr_end_position + 2, tokens.length));
+      symbol = tokens[left_expr_end_position + 1];
+    }
+    return {
+      name: symbol,
+      left: left,
+      right: right
+    };
   }
 }
 
-Parse(tokens);
+function FindCloseParenthesisPosition(tokens) {
+  var position, parenthesis_count;
+  parenthesis_count = 0;
+  for (var i = 0; i < tokens.length; i++) {
+    if (tokens[i] == "(") {
+      parenthesis_count += 1;
+    } else if (tokens[i] == ")") {
+      parenthesis_count -= 1;
+    }
+    if (parenthesis_count == 0) {
+      position = i;
+      break
+    }
+  }
+  return position;
+}
+
+x = Parse(tokens);
+y = Parse(another_tokens);
+z = Parse(tokens2);
+console.log(x);
+console.log(y);
+console.log(z);
