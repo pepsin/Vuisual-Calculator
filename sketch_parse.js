77567dc01dcbf7b831b21f57461327d5728d390e
pepsin <yechunlin@admaster.com.cn>
2014-04-07 20:22:11 +0800

    Organize the Parser Structure

--- sketch_parse.js
+++ sketch_parse.js
@@ -1,46 +1,70 @@
 var tokens = ["(","x", "+", "1", ")", "+", "(", "POW", "(", "x", ",", "000.1", ")", "+", "x", ")", "*", "30.2", "+", "301" ];
 var another_tokens = ["x", "+", "(", "POW", "(", "x", ",", "000.1", ")", "+", "x", ")", "*", "30.2", "+", "301" ];
 var tokens2 = ["POW", "(", "x", ",", "000.1", ")", "+", "x", "*", "30.2", "+", "301" ];
-LITERALS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
-NUMBER = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+
 IDENTIFER = 0;
 ARITHMETIC = 1;
 NUMBER = 2;
+BUILDIN_FUNCTION = 3;
+USER_FUNCTION = 4;
+
 function Parse(tokens) {
-  var left, right, symbol;
   if (tokens.length < 2) {
     return tokens[0];
   } else {
     if (tokens[0] == "POW") {
-      var pow_end_position = tokens.indexOf(")")
-      var the_whole_pow_expr = tokens.slice(0, pow_end_position);
-      var comma_position = the_whole_pow_expr.indexOf(",");
-      left = {
-        name: "POW",
-        first: Parse(the_whole_pow_expr.slice(2, comma_position)),
-        second: Parse(the_whole_pow_expr.slice(comma_position + 1, the_whole_pow_expr.length))
-      }
-      var right_expr = tokens.slice(pow_end_position + 2, tokens.length);
-      right = Parse(right_expr);
-      symbol = tokens[pow_end_position + 1];
+      return FunctionResolver(tokens);
     } else if (tokens[0] != "(") {
-      var left_expr = [tokens[0]];
-      var right_expr = tokens.slice(2, tokens.length);
-      left = Parse(left_expr);
-      right = Parse(right_expr);
-      symbol = tokens[1];
+      return NormalResolver(tokens);
     } else {
-      var left_expr_end_position = FindCloseParenthesisPosition(tokens.slice(0, tokens.length));
-      left = Parse(tokens.slice(1, left_expr_end_position));
-      right = Parse(tokens.slice(left_expr_end_position + 2, tokens.length));
-      symbol = tokens[left_expr_end_position + 1];
+      return ParenthesisResolver(tokens);
     }
-    return {
-      name: symbol,
-      left: left,
-      right: right
-    };
   }
+}
+
+function FunctionResolver(tokens) {
+  var pow_end_position = tokens.indexOf(")")
+  var the_whole_pow_expr = tokens.slice(0, pow_end_position);
+  var comma_position = the_whole_pow_expr.indexOf(",");
+  left = {
+    symbol: "POW",
+    type: BUILDIN_FUNCTION,
+    first: Parse(the_whole_pow_expr.slice(2, comma_position)),
+    second: Parse(the_whole_pow_expr.slice(comma_position + 1, the_whole_pow_expr.length))
+  }
+  var right_expr = tokens.slice(pow_end_position + 2, tokens.length);
+  right = Parse(right_expr);
+  symbol = tokens[pow_end_position + 1];
+  return {
+    left: left,
+    right: right,
+    symbol: tokens[pow_end_position + 1]
+  }
+}
+
+function ParenthesisResolver(tokens) {
+  var left_expr, right_expr, symbol, left_expr_end_position;
+  left_expr_end_position = FindCloseParenthesisPosition(tokens.slice(0, tokens.length));
+  left_expr = tokens.slice(1, left_expr_end_position);
+  right_expr = tokens.slice(left_expr_end_position + 2, tokens.length);
+  symbol = tokens[left_expr_end_position + 1];
+  return {
+    left: Parse(left_expr),
+    right: Parse(right_expr),
+    symbol: symbol
+  };
+}
+
+function NormalResolver(tokens) {
+  var left_expr, right_expr, symbol;
+  left_expr = [tokens[0]];
+  right_expr = tokens.slice(2, tokens.length);
+  symbol = tokens[1];
+  return {
+    left: Parse(left_expr),
+    right: Parse(right_expr),
+    symbol: symbol
+  };
 }
 
 function FindCloseParenthesisPosition(tokens) {
@@ -63,6 +87,7 @@ function FindCloseParenthesisPosition(tokens) {
 x = Parse(tokens);
 y = Parse(another_tokens);
 z = Parse(tokens2);
+console.log("-----------------------------------分隔----------------------------------------")
 console.log(x);
 console.log(y);
 console.log(z);
