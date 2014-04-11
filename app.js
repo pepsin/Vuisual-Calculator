574f2457704109439f19a1451d750e5af02dfc0f
pepsin <yechunlin@admaster.com.cn>
2014-04-11 11:35:10 +0800

    Add function definition parsing
    There’s a bug that using left, right will causing the AST level has
    malposition

--- app.js
+++ app.js
@@ -36,43 +36,53 @@ function Tokenize(string) {
   }
   return result;
 }
+var FUNCTIONS = {};
 function Parse(tokens) {
+  TYPE = { IDENTIFER: "IDENTIFER", OPERATOR: "OPERATOR" };
   if (tokens.length < 2) {
     return tokens[0];
   } else {
-    if (tokens[0] == "POW") {
-      return FunctionResolver(tokens);
-    } else if (tokens[0] != "(") {
-      return NormalResolver(tokens);
+    if (tokens[0]["token"] != "(") {
+      if (tokens[0]["type"] == TYPE.IDENTIFER) {
+        if (tokens[tokens.length - 1]["token"] == "=") {
+        
+        } else {
+          if (tokens[1]["token"] == "(") {
+            var definition_tokens, left_tokens, end_parenthesis;
+            end_parenthesis = FindCloseParenthesisPosition(tokens.slice(1, tokens.length));
+            left_tokens = tokens.slice(end_parenthesis + 2, tokens.length);
+            console.log(left_tokens);
+            definition_tokens = tokens.slice(0, end_parenthesis + 2);
+            return FunctionDefineResolver(definition_tokens, left_tokens);
+          } else {
+            return NormalResolver(tokens);
+          }
+        }
+      }
     } else {
       return ParenthesisResolver(tokens);
     }
   }
 }
 
-function FunctionResolver(tokens) {
-  var pow_end_position = tokens.indexOf(")")
-  var the_whole_pow_expr = tokens.slice(0, pow_end_position);
-  var comma_position = the_whole_pow_expr.indexOf(",");
-  left = {
-    symbol: "POW",
-    type: BUILDIN_FUNCTION,
-    first: Parse(the_whole_pow_expr.slice(2, comma_position)),
-    second: Parse(the_whole_pow_expr.slice(comma_position + 1, the_whole_pow_expr.length))
+function FunctionDefineResolver(definition_tokens, left_tokens) {
+  var definition;
+  if (left_tokens[0]["token"] == "=") {
+    definition = Parse(left_tokens.slice(1, left_tokens.length));
+  } else {
+    definition = "";
   }
-  var right_expr = tokens.slice(pow_end_position + 2, tokens.length);
-  right = Parse(right_expr);
-  symbol = tokens[pow_end_position + 1];
   return {
-    left: left,
-    right: right,
-    symbol: tokens[pow_end_position + 1]
+    symbol: definition_tokens[0]["token"],
+    type: "function",
+    variables: definition_tokens.slice(2, definition_tokens.length - 1).filter(function(el) { return el.type != TYPE.OPERATOR; }),
+    definition: definition
   }
 }
 
 function ParenthesisResolver(tokens) {
   var left_expr, right_expr, symbol, left_expr_end_position;
-  left_expr_end_position = FindCloseParenthesisPosition(tokens.slice(0, tokens.length));
+  left_expr_end_position = FindCloseParenthesisPosition(tokens);
   left_expr = tokens.slice(1, left_expr_end_position);
   right_expr = tokens.slice(left_expr_end_position + 2, tokens.length);
   symbol = tokens[left_expr_end_position + 1];
@@ -99,9 +109,9 @@ function FindCloseParenthesisPosition(tokens) {
   var position, parenthesis_count;
   parenthesis_count = 0;
   for (var i = 0; i < tokens.length; i++) {
-    if (tokens[i] == "(") {
+    if (tokens[i]["token"] == "(") {
       parenthesis_count += 1;
-    } else if (tokens[i] == ")") {
+    } else if (tokens[i]["token"] == ")") {
       parenthesis_count -= 1;
     }
     if (parenthesis_count == 0) {
@@ -114,10 +124,11 @@ function FindCloseParenthesisPosition(tokens) {
 var tokens = Tokenize("aFunction(x, y, z) = x + (POW(x, 20.1) + x) * 30.2 + 301");
 var tokens2 = Tokenize("aFunction(x, y, z) = x + (POW(x, 20.1) + x) * 30.2 + 301 / 31 + ((((((30))))))");
 //console.log(tokens);
-print(tokens2);
+//print(tokens2);
 
 x = Parse(tokens);
-print(x);
+console.log(x);
+//print(x);
 //y = Parse(another_tokens);
 //z = Parse(tokens2);
 //console.log("-----------------------------------分隔----------------------------------------");
